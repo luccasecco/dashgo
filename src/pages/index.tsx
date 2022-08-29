733563cb344f4e3eb24ad56d31aa7660a9acb2c1
luccasecco <luccasecco@hotmail.com>
2022-08-29 09:06:51 -0300

    add componente input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
-import { Button, Flex, Input, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+import { Button, Flex, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
 const Home: NextPage = () => {
   return (
@@ -19,36 +20,8 @@ const Home: NextPage = () => {
         borderRadius={8}
       >
        <Stack spacing={4}>
-       <FormControl>
-        <FormLabel htmlFor="email">E-mail</FormLabel>
-        <Input 
-          name="email" 
-          id="email"
-          type="email" 
-          focusBorderColor="pink.500"
-          bgColor="gray.900"
-          variant="filled"
-          _hover={{
-            bgColor: 'gray.900'
-          }}
-          size="lg"
-        />
-        </FormControl>
-        <FormControl>
-        <FormLabel htmlFor="password">Senha</FormLabel>
-        <Input 
-          name="password"
-          id="password" 
-          type="password" 
-          focusBorderColor="pink.500"
-          bgColor="gray.900"
-          variant="filled"
-          _hover={{
-            bgColor: 'gray.900'
-          }}
-          size="lg"
-        />
-        </FormControl>
+        <Input name="email" type="email" label="E-mail"/>
+        <Input name="password" type="password" label="Senha"/>
         </Stack>
 
         <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
