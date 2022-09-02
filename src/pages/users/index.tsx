91483281f234dc168463d8c50b8f4c93f36708bf
luccasecco <luccasecco@hotmail.com>
2022-09-02 14:21:04 -0300

    add paginação

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -118,7 +118,11 @@ export default function UserList() {
           </Table>
 
 
-        <Pagination />
+        <Pagination 
+          totalCountOfRegisters={200}
+          currentPage={6}
+          onPageChange={() => {}}
+        />
         </>
           )}
 
